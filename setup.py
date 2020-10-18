b8cdcd64aa5b30c18619eb1d0ec601041113226d
clericpy <clericpy@gmail.com>
2020-10-19 00:10:37 +0800

    remove python2 support

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ import re
 from setuptools import find_packages, setup
 """
 linux:
-rm -rf "dist/*";rm -rf "build/*";python3 setup.py bdist_wheel;python2 setup.py bdist_wheel;twine upload "dist/*;rm -rf "dist/*";rm -rf "build/*""
+rm -rf "dist/*";rm -rf "build/*";python3 setup.py bdist_wheel;twine upload "dist/*;rm -rf "dist/*";rm -rf "build/*""
 win32:
-rm -rf dist;rm -rf build;python3 setup.py bdist_wheel;python2 setup.py bdist_wheel;twine upload "dist/*";rm -rf dist;rm -rf build;rm -rf *.egg-info
+rm -rf dist;rm -rf build;python3 setup.py bdist_wheel;twine upload "dist/*";rm -rf dist;rm -rf build;rm -rf *.egg-info
 """
 
 with codecs.open("README.md", encoding="u8") as f:
