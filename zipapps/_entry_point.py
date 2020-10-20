86eab8f470265b87fe801ae2f8d6cd72356229fb
clericpy <clericpy@gmail.com>
2020-10-21 01:13:49 +0800

    now we can package C-libs with unzip

--- zipapps/_entry_point.py
+++ zipapps/_entry_point.py
@@ -1,14 +1,53 @@
+# -*- coding: utf-8 -*-
 import os
-import subprocess
 import sys
 from pathlib import Path
+from shutil import rmtree
+from zipfile import ZipFile
 
 
 def main():
-    """run python file with current PYTHONPATH"""
-    # default entry point, used like venv
-    shell_args = [sys.executable] + sys.argv[1:]
+    """run with current PYTHONPATH"""
     # env of Popen is not valid for win32 platform, use os.environ instead.
     # PYTHONPATH=./app.pyz
-    os.environ['PYTHONPATH'] = str(Path(__file__).parent.absolute())
-    subprocess.Popen(shell_args, shell=False).wait()
+    zip_file_path = str(Path(__file__).parent.absolute())
+    python_path_list = [zip_file_path]
+    unzip = r'''{unzip}'''
+    if unzip:
+        _temp_folder = os.path.splitext(zip_file_path)[0] + '_'
+        python_path_list.insert(0, _temp_folder)
+        _temp_folder_path = Path(_temp_folder)
+        if not (_temp_folder_path / '_zip_time_{ts}').is_file():
+            # check timestamp different, need to refresh _temp_folder
+            for _ in range(3):
+                try:
+                    if not _temp_folder_path.is_dir():
+                        break
+                    rmtree(_temp_folder_path)
+                except FileNotFoundError:
+                    break
+            _temp_folder_path.mkdir()
+            _need_unzip_names = unzip.split(',')
+            _need_unzip_names.append('_zip_time_{ts}')
+            with ZipFile(zip_file_path, "r") as zf:
+                for member in zf.infolist():
+                    file_dir_name = os.path.splitext(member.filename.split('/')[0])[0]
+                    if file_dir_name in _need_unzip_names:
+                        zf.extract(member, path=str(_temp_folder_path.absolute()))
+
+    sep = ';' if sys.platform == 'win32' else ':'
+    os.environ['PYTHONPATH'] = sep.join(python_path_list)
+
+    shell_args = [sys.executable] + sys.argv[1:]
+    has_main = {has_main}
+    if has_main:
+        for p in python_path_list[::-1]:
+            sys.path.insert(0, p)
+        pass{import_main}{run_main}
+    else:
+        from subprocess import Popen
+        Popen(shell_args, shell={shell}).wait()
+
+
+if __name__ == "__main__":
+    main()
