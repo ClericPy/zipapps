654865147b0a9d04a8cabd75937b9e1abe220d4b
clericpy <clericpy@gmail.com>
2020-11-23 01:21:22 +0800

    support from zipapps import activate

---
+++ zipapps/activate_zipapps.py
@@ -0,0 +1,11 @@
+import pathlib
+import zipimport
+from warnings import warn
+
+
+def activate(path=None):
+    path = path or pathlib.Path(__file__).parent.absolute()
+    try:
+        zipimport.zipimporter(path).load_module("ensure_zipapps")
+    except ImportError as err:
+        warn(f'activate failed for {err!r}')
