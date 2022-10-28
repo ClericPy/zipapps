39d94b93209a224430cb81d020dce94f022368ba
lidong <lidong@cmss.chinamobile.com>
2022-10-28 22:59:53 +0800

    add -quiet; add python3.11 test

--- zipapps/main.py
+++ zipapps/main.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from pkgutil import get_data
 from zipfile import ZIP_DEFLATED, ZIP_STORED, BadZipFile, ZipFile
 
-__version__ = '2022.08.29'
+__version__ = '2022.10.28'
 
 
 def get_pip_main(ensurepip_root=None):
