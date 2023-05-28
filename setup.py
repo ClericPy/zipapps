987c93dc9084b6780512222200afad8fa24e3af9
lidong <lidong@cmss.chinamobile.com>
2023-05-28 12:25:04 +0800

    fix build status icon

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     author="ClericPy",
     author_email="clericpy@gmail.com",
