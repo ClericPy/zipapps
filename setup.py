d796ffd5a7be60b44901c06703a42c1f6fc0d86e
Clericpy <clericpy@gmail.com>
2024-06-04 23:48:59 +0800

    remove 3.6 test

--- setup.py
+++ setup.py
@@ -33,12 +33,11 @@ setup(
     license="MIT License",
     package_data={"zipapps": ["*.template"]},
     py_modules=["zipapps"],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
