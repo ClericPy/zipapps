e25505d9146bd5c7feaefd5883962c0479edbd03
Clericpy <clericpy@gmail.com>
2024-06-05 00:05:07 +0800

    use flit for python3.12 pip

--- zipapps/__init__.py
+++ zipapps/__init__.py
@@ -3,3 +3,4 @@ from .main import create_app, __version__, ZipApp, pip_install_target
 from .activate_zipapps import activate
 
 __all__ = ["create_app", "activate", "__version__", "ZipApp", "pip_install_target"]
+__doc__ = "Package your python code into one zip file, even a virtual environment."
